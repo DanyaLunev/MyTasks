f51871f9a09ad750c7a1f9220c516a637262ec34
Lunev Daniil <denspeedcuber2006@gmail.com>
2020-07-13 11:47:16 +0300

    first task is ready

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+        int c = 1;
+
+        System.out.print(a);
+        System.out.print("  ");
+        System.out.print(b);
+        System.out.print("  ");
+        System.out.print(c);
+    }
+}
