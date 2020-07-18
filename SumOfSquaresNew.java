9cdd42e299e9ba0ee9b71b2044c211a85630db33
Lunev Daniil <denspeedcuber2006@gmail.com>
2020-07-18 17:11:13 +0300

    add SumOfSquaresNew

---
+++ SumOfSquaresNew.java
@@ -0,0 +1,62 @@
+
+
+public class SumOfSquaresNew {
+
+    //Print one number
+    public static void printNumber(int x) {
+        System.out.print(x);
+    }
+
+    //Elevates to a power
+    public static int pow2(int x) {
+        return (int) Math.pow(x, 2);
+    }
+
+    //Fold two numbers
+    public static int addition(int x, int y) {
+        return x + y;
+    }
+
+    //Find two MAX Numbers
+    public static int[] maxNumbers(int arr[]) {
+
+        int[] result = new int[2];
+        int firstMax = arr[0];
+        int secondMax = arr[1];
+
+        //Find the largest number
+        if (firstMax < secondMax) {
+            firstMax = arr[1];
+            secondMax = arr[0];
+        }
+
+        for (int i = 3; i < arr.length - 1; i++) {
+            if (arr[i] > firstMax) {
+                secondMax = firstMax;
+                firstMax = arr[i];
+            }
+        }
+
+        result[0] = firstMax;
+        result[1] = secondMax;
+
+        return result;
+    }
+
+    public static int powSum(int x, int y) {
+        x = pow2(x);
+        y = pow2(y);
+
+        return addition(x, y);
+    }
+
+    public static void main(String[] args) {
+        int[] inputs = {1, 7, 4, 3, 6, 5, 2, 1};
+        int[] maximums = new int[2];
+
+        maximums = maxNumbers(inputs);
+        int powSumma = powSum(maximums[0], maximums[1]);
+
+        printNumber(powSumma);
+    }
+}
