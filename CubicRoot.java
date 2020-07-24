939aba8f45255427317acd8d7eafe93e91afe48d
Lunev Daniil <denspeedcuber2006@gmail.com>
2020-07-24 10:30:51 +0300

    update CubicRoot

--- CubicRoot.java
+++ CubicRoot.java
@@ -6,18 +6,18 @@ class CubicRoot {
     }
 
     public static double cubrt(double number) {
-        double start = 2;
+        double start = 1;
         double cubrt = 0;
         int delta = 10;
 
         for (int i = 0; i < delta; i++) {
-            cubrt = (number / start + 2 * start) / 3;
-            start = (number / start + 2 * start) / 3;
+            cubrt = (number / Math.pow(start, 2) + 2 * start) / 3;
+            start = (number / Math.pow(start, 2) + 2 * start) / 3;
         }
         return cubrt;
     }
 
     public static void main(String[] args) {
-        printNumber(cubrt(81.0));
+        printNumber(cubrt(23.0));
     }
 }
